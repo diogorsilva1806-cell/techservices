dd75081bddb4bed5f8c08ff4b9ae20ba7e0fe0a3
diogorsilva1806-cell <diogorsilva1806@gmail.com>
2026-07-30 15:46:35 +0100

    terceiro comit do aluno

--- src/models/cliente.py
+++ src/models/cliente.py
@@ -1,15 +1,41 @@
 class Cliente:
 
-    def __init__(
-        self,
-        nome,
-        email,
-        telefone="",
-        id_cliente=None,
-        status=1
-    ):
-        self.id_cliente = id_cliente
-        self.nome = nome
-        self.email = email
-        self.telefone = telefone
-        self.status = status
+
+            def __init__(self, id_cliente,  nome, email, telefone, status):
+                self.__id_cliente = id_cliente
+                self.__nome = nome
+                self.__email = email
+                self.__telefone = telefone
+                self.__status = status
+
+            # Getters
+            def get_id_cliente(self):
+                return self.__id_cliente
+
+            def get_nome(self):
+                return self.__nome
+
+            def get_email(self):
+                return self.__email
+
+            def get_telefone(self):
+                return self.__telefone
+
+            def get_status(self):
+                return self.__status
+
+            # Setters
+            def set_id_cliente(self, novo_id_cliente):
+                self.__id_cliente = novo_id_cliente
+
+            def set_nome(self, novo_nome):
+                self.__nome = novo_nome
+
+            def set_email(self, novo_email):
+                self.__email = novo_email
+
+            def set_telefone(self, novo_telefone):
+                self.__telefone = novo_telefone
+
+            def set_status(self, novo_status):
+                    self.__status = novo_status
